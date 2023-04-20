ab23d8c8b4cf2e612c8bba926d55257561294752
Todd0554 <toddy9229@outlook.com>
2023-04-20 10:25:34 +1000

    modify form problem

--- src/components/HomePageComponents/6-Contacts/Contacts.js
+++ src/components/HomePageComponents/6-Contacts/Contacts.js
@@ -3,21 +3,20 @@ import "./style.css";
 
 function Contacts() {
   return (
-
-      <div className='contacts'>
-        <h2>Contact us to find out more</h2>
-        <p>One of our friendly staff members will contact you within 24 hours</p>
-        <div className='row row4' >
-          <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-            <input type="text" className="form-control" placeholder="My name" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-          </div>
-          <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-            <input type="text" className="form-control" placeholder="Email address" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-          </div>
-          <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-            <input type="text" className="form-control" placeholder="School name" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-          </div>
-          <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+    <div className='contacts'>
+      <h2>Contact us to find out more</h2>
+      <p>One of our friendly staff members will contact you within 24 hours</p>
+      <form className='row row4' >
+        <div className="form-group col-sm-12 col-md-12 col-lg-6 col-xl-6">
+          <input type="text" className="form-control" placeholder="My name" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+        </div>
+        <div className="form-group col-sm-12 col-md-12 col-lg-6 col-xl-6">
+          <input type="text" className="form-control" placeholder="Email address" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+        </div>
+        <div className="form-group col-sm-12 col-md-12 col-lg-6 col-xl-6">
+          <input type="text" className="form-control" placeholder="School name" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+        </div>
+        <div className="form-group col-sm-12 col-md-12 col-lg-6 col-xl-6">
           <select class="form-control" id="exampleFormControlSelect1">
             <option>Australia</option>
             <option>USA</option>
@@ -27,15 +26,13 @@ function Contacts() {
           </select>
           <div className="form-btns">
             <a href="#">Add message (optional)</a>
-            <div className="btn2-con">
+            <div className="btn2-con" type="submit">
               Send
             </div>
           </div>
-          </div>
         </div>
-
-      </div>
-
+      </form>
+    </div>
   )
 }
 
